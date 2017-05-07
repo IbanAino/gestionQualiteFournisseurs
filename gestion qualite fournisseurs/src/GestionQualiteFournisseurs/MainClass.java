3bb43bdee4d93d3b5e09f9f356fc7f1859d6625a
ESTIA\i.fernandez <iban.fernandez@laposte.net>
2017-05-07 22:46:25 +0200

    Copie du projet.

---
+++ gestion qualite fournisseurs/src/GestionQualiteFournisseurs/MainClass.java
@@ -0,0 +1,67 @@
+package GestionQualiteFournisseurs;
+
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+
+public class MainClass extends Application{
+
+	public static void main(String[] args) {
+		System.out.println("Software launched");
+		
+		/*
+		// CONNECTION TO THE DATABASE
+		
+		try{
+		      Class.forName("com.mysql.jdbc.Driver").newInstance(); // load the driver
+		      
+		      String url = "jdbc:mysql://localhost/logiciel_conformite";
+		      String user = "root";
+		      String passwd = "";
+		      
+		      Connection conn = (Connection) DriverManager.getConnection(url, user, passwd); // connection
+		      
+		      System.out.println("connection from database OK");		      
+		}
+	    catch (ClassNotFoundException ex) {System.err.println(ex.getMessage());}
+	    catch (IllegalAccessException ex) {System.err.println(ex.getMessage());}
+	    catch (InstantiationException ex) {System.err.println(ex.getMessage());}
+	    catch (SQLException ex)			  {System.err.println(ex.getMessage());}
+		*/
+		
+		launch(args); // call the start function
+	}
+	
+	
+	// LAUNCH THE FIRST WINDOW
+	
+	public void start(Stage primaryStage) {
+		// Charger le fichier XML, en capturant une erreur si ça ne marche pas.
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/PageConnexion/PageConnexion.fxml"));
+			Scene scene = new Scene(root, 800, 600, Color.BLACK);
+			primaryStage.setScene(scene);
+			primaryStage.sizeToScene();
+			primaryStage.show();
+			
+			System.out.println("first window SUCCESS");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			System.out.println("first window ERROR");
+		}
+	}
+}
---
+++ gestion qualite fournisseurs/src/PageConnexion/PageConnexionController.java
@@ -0,0 +1,71 @@
+package PageConnexion;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.ResultSetMetaData;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class PageConnexionController {
+	
+	// ATTRIBUTS
+	Connection conn;
+	@FXML private TextField name;
+	
+	// CONSTRUCTOR
+	public PageConnexionController(){
+		System.out.println("PageConnexionController constructor called");
+		
+		// CONNECTION TO THE DATABASE
+		
+		try{
+		      Class.forName("com.mysql.jdbc.Driver").newInstance(); // load the driver
+		      
+		      String url = "jdbc:mysql://localhost/logiciel_conformite";
+		      String user = "root";
+		      String passwd = "";
+		      
+		      /*Connection*/ conn = (Connection) DriverManager.getConnection(url, user, passwd); // connection
+		      
+		      System.out.println("connection from database OK");		      
+		}
+	    catch (ClassNotFoundException ex) {System.err.println(ex.getMessage());}
+	    catch (IllegalAccessException ex) {System.err.println(ex.getMessage());}
+	    catch (InstantiationException ex) {System.err.println(ex.getMessage());}
+	    catch (SQLException ex)	{System.err.println(ex.getMessage());}
+	}
+	
+	@FXML
+	private void buttonConnection(ActionEvent event){
+		System.out.println("buttonConnexion clicked");
+		
+		// READ THE INPUT DATAS
+		System.out.println(name.getText());
+
+		
+		
+		// ASK THE DATABASE
+		
+		try{
+			Statement state = conn.createStatement();
+
+		    ResultSet result = state.executeQuery("SELECT * FROM employe");// L'objet ResultSet contient le résultat de la requête SQL
+			    while (result.next())
+			    {
+			    	String name = result.getString("nom");
+			    	System.out.println(name);
+			    }		
+		}
+		catch (SQLException ex) {System.err.println(ex.getMessage());}
+		
+		// COMPARE INPUT DATAS WITH THE DATABASE DATAS
+		
+	}
+
+}
